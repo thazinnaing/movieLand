fc85f232b02f976370960baf137f31ffd68fdf86
Thazin <thazinnaingmm@gmail.com>
2023-12-25 21:49:54 +0630

    fix: fix some errors

--- src/components/movieCard.jsx
+++ src/components/movieCard.jsx
@@ -1,25 +1,30 @@
-import './movieCard.css'
+import "./movieCard.css";
 
-const MovieCard=({props})=>{
-    return(
-        <div className="movie">
-        <div className="year">
-            <p>{props.Year}</p>
-        </div>
+// eslint-disable-next-line react/prop-types
+const MovieCard = ({ props }) => {
+  return (
+    <div className="movie">
+      <div className="year">
+        <p>{props.Year}</p>
+      </div>
 
-        <div className="cardImage">
-            <img src={props.Poster !== 'N/A' ? props.Poster : 'https://via.placeholder.com/3'} alt={props.Title}/>
-        </div>
+      <div className="cardImage">
+        <img
+          src={
+            props.Poster !== "N/A"
+              ? props.Poster
+              : "https://via.placeholder.com/3"
+          }
+          alt={props.Title}
+        />
+      </div>
 
-        <div className="description">
-            <h4>{props.Type}</h4>
-            <h3>{props.Title}</h3>
-        </div>
+      <div className="description">
+        <h4>{props.Type}</h4>
+        <h3>{props.Title}</h3>
+      </div>
     </div>
-
-    )
-    
-
-}
+  );
+};
 
 export default MovieCard;
